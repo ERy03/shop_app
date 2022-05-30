0cf7dc8241b9d62993c1cc432a5d941256b3190e
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-30 17:07:30 +0900

    provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/products.dart';
 import 'package:shop_app/screens/products_overview_screen.dart';
 
 import 'screens/product_detail_screen.dart';
@@ -8,16 +10,19 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        fontFamily: "Lato",
+    return ChangeNotifierProvider(
+      create: (context) => Products(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          fontFamily: "Lato",
+        ),
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
-      },
     );
   }
 }
