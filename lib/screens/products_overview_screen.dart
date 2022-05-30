e4ae77c591d4407a6d417e82a79b0d1c73b3ca63
Ryota Enoki <ryoenoki2013@gmail.com>
2022-05-30 17:09:20 +0900

    extracted widget

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -49,20 +49,34 @@ class ProductsOverviewScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text("MyShop"),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10.0),
-        itemCount: loadedProducts.length,
-        itemBuilder: (context, index) => ProductItem(
-          id: loadedProducts[index].id,
-          title: loadedProducts[index].title,
-          imageUrl: loadedProducts[index].imageUrl,
-        ),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2, // Control how many items you want across the grid
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10, // vertical space between columns
-          mainAxisSpacing: 10, // Horizontal space between rows
-        ),
+      body: ProductsGrid(loadedProducts: loadedProducts),
+    );
+  }
+}
+
+class ProductsGrid extends StatelessWidget {
+  const ProductsGrid({
+    Key? key,
+    required this.loadedProducts,
+  }) : super(key: key);
+
+  final List<Product> loadedProducts;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: loadedProducts.length,
+      itemBuilder: (context, index) => ProductItem(
+        id: loadedProducts[index].id,
+        title: loadedProducts[index].title,
+        imageUrl: loadedProducts[index].imageUrl,
+      ),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2, // Control how many items you want across the grid
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 10, // vertical space between columns
+        mainAxisSpacing: 10, // Horizontal space between rows
       ),
     );
   }
