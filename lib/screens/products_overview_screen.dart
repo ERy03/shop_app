bcb824453ef097ec285b05c7c2cb302a029188a0
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-01 11:03:41 +0900

    popupmenu button

--- lib/screens/products_overview_screen.dart
+++ lib/screens/products_overview_screen.dart
@@ -10,6 +10,24 @@ class ProductsOverviewScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("MyShop"),
+        actions: <Widget>[
+          PopupMenuButton(
+            onSelected: (int selectedValue) {
+              print(selectedValue);
+            },
+            icon: const Icon(Icons.more_vert),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: 0,
+                child: Text("Only Favorites"),
+              ),
+              const PopupMenuItem(
+                value: 1,
+                child: Text("Show All"),
+              ),
+            ],
+          ),
+        ],
       ),
       body: ProductsGrid(),
     );
