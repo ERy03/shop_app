7cc8b92f83efe6bd25ff181ad95ed522fcc7fc6d
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-01 15:47:15 +0900

    cart provider

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 import '../providers/product.dart';
 import '../screens/product_detail_screen.dart';
 
@@ -15,6 +16,7 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final product = Provider.of<Product>(context, listen: false);
+    final cart = Provider.of<Cart>(context, listen: false);
     // final product = context.read<Product>();
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
