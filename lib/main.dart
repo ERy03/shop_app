3f8eff8a86dec7f3dad76bca238695e9af77acaa
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-01 16:50:03 +0900

    cart screen and route

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:shop_app/screens/cart_screen.dart';
 import './providers/products.dart';
 
 import './screens/product_detail_screen.dart';
@@ -29,6 +30,7 @@ class MyApp extends StatelessWidget {
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+          CartScreen.routeName: (context) => CartScreen(),
         },
       ),
     );
--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/cart.dart';
 
 class CartScreen extends StatelessWidget {
+  static const routeName = "/cart";
   const CartScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final cart = Provider.of<Cart>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Cart"),
@@ -25,7 +29,8 @@ class CartScreen extends StatelessWidget {
                     width: 10,
                   ),
                   Chip(
-                    label: Text(),
+                    label: Text("\$${cart.totalAmount}"),
+                    backgroundColor: Theme.of(context).colorScheme.secondary,
                   ),
                 ],
               ),
