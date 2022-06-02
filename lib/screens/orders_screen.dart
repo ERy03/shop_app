e85e0d0bfc0a0c6b234090a8854b266b5650a796
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-02 14:30:02 +0900

    orders screen: list view builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../providers/orders.dart';
 
 class OrdersScreen extends StatelessWidget {
   const OrdersScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Your Orders"),
       ),
-      body: ,
+      body: ListView.builder(
+        itemCount: orderData.orders.length,
+        itemBuilder: itemBuilder,
+      ),
     );
   }
 }
