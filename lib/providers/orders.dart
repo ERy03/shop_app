46fca96d25bba1661dbd6836cf4de4634dbb2765
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-02 11:53:49 +0900

    orders.dart: adding order

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -22,4 +22,19 @@ class Orders with ChangeNotifier {
   List<OrderItem> get orders {
     return [..._orders];
   }
+
+  void addOrder(
+    List<CartItem> cartProducts,
+    double total,
+  ) {
+    _orders.insert(
+      0,
+      OrderItem(
+        id: DateTime.now().toString(),
+        amount: total,
+        products: cartProducts,
+        dateTime: DateTime.now(),
+      ),
+    );
+  }
 }
