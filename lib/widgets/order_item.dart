bafba2e27e2898538c5e990f874ecce377717f77
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-02 16:11:56 +0900

    order item: new variable called expanded created

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -12,6 +12,7 @@ class OrderItem extends StatefulWidget {
 }
 
 class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
