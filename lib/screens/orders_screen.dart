90ed230b1defe511b45e3fb36775999bb53b73ad
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-02 14:25:56 +0900

    orders screen: app bar

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -5,6 +5,10 @@ class OrdersScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Orders"),
+      ),
+    );
   }
 }
