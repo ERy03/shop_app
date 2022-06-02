5c42fdea22edf33ba758715325faca1e33d3aa10
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-02 14:42:38 +0900

    orders item list tile

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -4,5 +4,16 @@ class OrderItem extends StatelessWidget {
   const OrderItem({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(10.0),
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            title: Text("\$${}"),
+          )
+        ],
+      ),
+    );
+  }
 }
