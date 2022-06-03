4fa2f3c0c81da72eaec00e8638d4f16b1321ffac
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-03 10:38:30 +0900

    user products item basic structure

--- lib/widgets/user_product_item.dart
+++ lib/widgets/user_product_item.dart
@@ -3,6 +3,13 @@ import 'package:flutter/material.dart';
 class UserProductItem extends StatelessWidget {
   const UserProductItem({Key? key}) : super(key: key);
 
+  final String title;
+  final String imageUrl;
+
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(),
+    );
+  }
 }
