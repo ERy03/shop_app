ccb2100c1f7fdd200f5ab470b5dd2f7f8897052a
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-03 11:16:54 +0900

    edit product screen: basic structure

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -9,5 +9,11 @@ class EditProductScreen extends StatefulWidget {
 
 class _EditProductScreenState extends State<EditProductScreen> {
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Edit Product"),
+      ),
+    );
+  }
 }
