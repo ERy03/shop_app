e7e94ae4e78a78563aac1c11bcc7b14c5db11e94
Ryota Enoki <ryoenoki2013@gmail.com>
2022-06-03 18:26:31 +0900

    edit product screen : init values

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -65,6 +65,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
       final productId = ModalRoute.of(context)!.settings.arguments as String;
       _editedProduct =
           Provider.of<Products>(context, listen: false).findById(productId);
+      _initValues = {
+        "title": _editedProduct.title,
+        "description": _editedProduct.description,
+        "price": _editedProduct.price.toString(),
+        "imageUrl": _editedProduct.imageUrl,
+      };
     }
     _isInit = false;
     super.didChangeDependencies();
